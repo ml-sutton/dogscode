98083b15e76f30e19e19225a99f3674b9c46f1bb
Madison Sutton <YNotMadisonUwU@gmail.com>
2025-07-10 16:53:16 +1000

    am I goated with the sauce?

--- code/src/components/resources/paginator.tsx
+++ code/src/components/resources/paginator.tsx
@@ -12,11 +12,14 @@ export const Paginator:React.FC<PaginatorProps> = ({count,pageSize,setPageNumber
 {
   const pagesNeeded = Math.ceil(count/pageSize);
   const pageNumbers = Array.from({length:pagesNeeded},(_,i)=>i+1);
+  const showingMin = (selected-1)*pageSize+1 
+  const remaining = count-(pageSize*selected)
+  const showingMax = remaining<pageSize?count:selected*pageSize
   return (
-    <div className="w-screen text-black flex justify-between px-6 py-4">
-      <div className=""><h1 className="text-lg">Count : {count}</h1></div>
+    <div className="w-screen text-black flex justify-between px-6 py-4 border-t-2 border-gray-300">
+      <div className=""><h1 className="text-lg">showing {showingMin} to {count>pageSize?showingMax:count} of {count}</h1></div>
       <div className="">
-        {pageNumbers.map((number, key)=>(<a className={`border-2 border-gray-300 p-2 mx-0.5 rounded-lg cursor-pointer ${number==selected?"bg-gray-200 border-gray-400":""}`}  onClick={()=>setPageNumber(number)} key={key}>{number}</a>))}
+        {pageNumbers.map((number, key)=>(<a className={`border-2 border-gray-300 p-2 mx-0.5 rounded-lg cursor-pointer shadow-sm ${number==selected?"bg-gray-200 border-gray-400":""}`}  onClick={()=>setPageNumber(number)} key={key}>{number}</a>))}
       </div>
     </div>
   )
