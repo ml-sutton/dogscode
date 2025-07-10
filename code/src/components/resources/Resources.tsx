a2c6a5a564455166fe2be220f93ce3b58eb28b81
Madison Sutton <YNotMadisonUwU@gmail.com>
2025-07-10 16:58:33 +1000

    I should change the eslint config

--- code/src/components/resources/Resources.tsx
+++ code/src/components/resources/Resources.tsx
@@ -1,7 +1,7 @@
 "use client"
 import { currentResources, Resource, ResourceType } from "@/models/resource"
 import { ResourceComponent } from "./Resource";
-import { SetStateAction, useState } from "react";
+import { useState } from "react";
 import { ResourceFilter } from "./ResourceFilter";
 import { ResourceBlurb } from "./ResourceBlurb";
 import { Paginator } from "./paginator";
