ee323f80486ab39cf81d8593ac5678ea89e9afba
Madison Sutton <YNotMadisonUwU@gmail.com>
2025-07-12 09:57:59 +1000

    will need to make this api route work later

--- code/src/app/api/resources/route.ts
+++ code/src/app/api/resources/route.ts
@@ -1,8 +1,6 @@
-// import {NextApiRequest} from 'next'
-// // time to use github as a cms
-// export async function GET(request:NextApiRequest)
-// {
-//   const response = await fetch("")
-//   console.log(await response.text())
-//   return new Response();
-// }
+import {NextApiRequest} from 'next'
+// time to use github as a cms
+export async function GET(request:NextApiRequest)
+{
+  return new Response();
+}
